a464295ea562459cdf7ee6df5d6209a161a026b3
FabienB4 <fabienb4.b4soft@gmail.com>
2015-05-23 17:33:03 -0600

    Allow meteor-jsdoc to work outside a git repository. closes #2

--- scripts/build_docs.sh
+++ scripts/build_docs.sh
@@ -6,7 +6,7 @@ cd <%= projectPath %>
 # Call git grep to find all js files with the appropriate comment tags,
 # and only then pass it to JSDoc which will parse the JS files.
 # This is a whole lot faster than calling JSDoc recursively.
-git grep -al "@summary" | xargs -L 10000 -t \
+git grep -al --no-index "@summary" | xargs -L 10000 -t \
     <%= nodePath %> \
     <%= jsdocPath %> \
     -t <%= jsdocTmplPath %> \
